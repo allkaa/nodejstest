17816729bfb06b003a90daebe3a46260c1433bb0
AKaarna <akaarna@debian930.AccountGt.net>
2018-09-30 12:12:04 +0300

    apply() method

--- iter_gen.js
+++ iter_gen.js
@@ -149,15 +149,21 @@ a; // "a"
 });
 
 
-function avg(...args) {
+function avg(...args) { // use rest parameter syntax.
   var sum = 0;
-  for (let value of args) {
+  for (let value of args) { // use for ... of loop.
     sum += value;
   }
   return sum / args.length;
 }
-ttt = avg(2, 3, 4, 5); // 3.5
-
+//ttt = avg(2, 3, 4, 5); // 3.5
+// use apply() with arbitraty params
+ttt = avg.apply(null, [2, 3, 4, 5]); // 3.5
+let numbers = [2,3,4,5];
+ttt = avg(numbers); // gives NaN
+ttt = avg(null, numbers); // gives NaN
+ttt = avg.apply(null, numbers); // gives 3.5
+ttt = avg(...numbers); //gives 3.5
 
 
 console.log('=========================================================');
