ead21de1b547b0d6fbb1558ea5097eb3c418ba10
AKaarna <akaarna@debian930.AccountGt.net>
2018-10-02 11:50:19 +0300

    func

--- funcs.js
+++ funcs.js
@@ -277,16 +277,11 @@ var factorial = function fac(n) {
   };
 ttt = factorial(3);
 
-var charsInBody = (function counter(elm) {
-  if (elm.nodeType == 3) { // TEXT_NODE
-    return elm.nodeValue.length;
-  }
-  var count = 0;
-  for (var i = 0, child; child = elm.childNodes[i]; i++) {
-    count += counter(child);
-  }
-  return count;
-})(document.body);
+// Using  IIFEs (Immediately Invoked Function Expressions).
+ttt = (function fac(n) {
+  return n < 2 ? 1 : n * fac(n - 1); 
+ })(3);
+
 
 console.log('=========================================================');
 dtVar = new Date();
