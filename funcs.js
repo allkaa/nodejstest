a65ed14b6086208a5f9c0b192014da761e9eca45
AKaarna <akaarna@debian930.AccountGt.net>
2018-10-14 11:11:42 +0300

    closures

--- funcs.js
+++ funcs.js
@@ -153,7 +153,7 @@ aaa = func();
 // Samples of using arrow function:
 let empty = (() => {})(); // An empty arrow function returns undefined
 
-aaa = (() => 'foobar')(); // Returns "foobar" (this is an Immediately Invoked Function Expression see 'IIFE' in glossary).
+aaa = (() => 'foobar')(); // Returns "foobar" (it is an Immediately Invoked Function Expression see 'IIFE' in glossary).
 
 var simple = a => a > 15 ? 15 : a;
 //var simple = (a) => {return a > 15 ? 15 : a;}; // same block body form.
@@ -521,38 +521,53 @@ aaa = f.call(b,222,333); // call function f with `this` as b and params as list,
   and global ones, NOT the ones from the scope in which the Function constructor was created.
   This is different from using eval with code for a function expression.
 */
-aaa = Function(); // empty Function object, methods thru Function.prototype.
+aaa1 = new Function(); // empty Function object - function anonymous(), methods used thru Function.prototype, closure created.
+// or aaa1 = Function(); // same result as above.
 let x = 10;
 function createFunction1() {
-    var x = 20;
-    // Constructor does not create closure.
-    //let f = new Function('return x;'); // this |x| refers global |x| - Reference Error x is not defined.
-    //return f();
-    let f = new Function('x','return x;'); // 20
-    return f(x);
+    let x = 20
+    // Nested Constructor does not create closure, at this point `this` is undefined,
+    let f = new Function('return x;'); // but inside f() 'this` will be global and |x| refers global |x| - Reference Error x is not defined.
+    return f();  // inside f() 'this` will be global and |x| refers global |x| - Reference Error x is not defined.
+    // But can work thru parameter:
+    //let f = new Function('x','return x;'); // 20 works thru parameter.
+    //return f(x);
 }
 function createFunction2() {
-    var x = 20;
-    // Declaration creates closure.
+    let x = 30;
+    // Nested Declaration creates closure that will be  kept.
     function f() {
         return x; // this |x| refers local |x| above
     }
     return f();
 }
-aaa = createFunction1(); // Reference Error x is not defined or 20.
-aaa = createFunction2(); // 20
+//aaa2 = createFunction1(); // Reference Error x is not defined or 20.
+aaa2 = createFunction2(); // 30 and closure will be created and saved.
 
-// Nested functions.
+// Nested functions and closures.
 // An important detail of nested functions in JavaScript is that they can access variables in their parent function's scope:
-function parentFunc() {
-  var a = 1;
+function parentFunc(x) {
+  var a = x;
   function nestedFunc() {
     var b = 4; // parentFunc can't use this
     return a + b; 
   }
-  return nestedFunc(); // 5
+  return nestedFunc();
 }
-aaa = parentFunc();
+parentFunc(1); // closure will be garbished.
+let aaax = parentFunc(1); // closure will be garbished.
+let aaay = parentFunc(2); // closure will be garbished.
+
+function makeFuncM() {
+  var name = 'Mozilla';
+  function displayName() {
+    var aaaTest = name;
+  }
+  return displayName;
+}
+
+aaax = makeFuncM(); // return function displayName() and it's closures.
+aaa3 = aaax();
 
 // Closures.
 // Whenever JavaScript executes a function, a 'scope' object is created to hold the local variables
@@ -565,8 +580,8 @@ function makeAdder(a) {
     return a + b;
   };
 }
-let aaax = makeAdder(5);
-let aaay = makeAdder(20);
+aaax = makeAdder(5); // return ref to function (b) with a=5 in closure.
+aaay = makeAdder(20); // return ref to function (b) with a=20 in closure.
 aaa = aaax(6); // 5(in closure) + 6
 aaa = aaay(7); // 20(in closure) + 7
 
@@ -577,12 +592,124 @@ Normally JavaScript's garbage collector would clean up the scope object created 
 but the returned function maintains a reference back to that scope object.
 As a result, the scope object will not be garbage-collected until there are no more references to the function object
 that makeAdder() returned.
+In essence, makeAdder is a function factory — it creates functions which can add a specific value to their argument.
 
 Scope objects form a chain called the scope chain, similar to the prototype chain used by JavaScript's object system.
 
 A closure is the combination of a function and the scope object in which it was created.
 Closures let you save state — as such, they can often be used in place of objects. 
 */
+
+// Module pattern using closures:
+// Creating object counter having "private" var privateCouter and changeBy(val) method in closure and
+// three "public" methods increment(), decrement() and value().
+var counter = (function() { // IIFE anonymous function creats closure with pirvateCountere and changeBy(val), returning Object.
+  var privateCounter = 0; // "private" variable property in closure.
+  function changeBy(val) { // "private" method in closure.
+    privateCounter += val;
+  }
+  return { // returns Object with three properties - increment, decrement and value.
+    increment: function() { // "public" method.
+      changeBy(1);
+    },
+    decrement: function() { // "public" method.
+      changeBy(-1);
+    },
+    value: function() { // "public" method.
+      return privateCounter;
+    }
+  };   
+})(); // Immediately Invoked Function Expression see 'IIFE'.
+
+aaa = counter.value(); // logs 0
+counter.increment();
+counter.increment();
+aaa = counter.value(); // logs 2
+counter.decrement();
+aaa = counter.value(); // logs 1
+/*
+Above shared lexical environment is created in the body of an anonymous function, which is executed as soon
+as it has been defined.
+The lexical environment contains two private items: a variable called privateCounter and a function called changeBy.
+Neither of these private items can be accessed directly from outside the anonymous function.
+Instead, they must be accessed by the three public functions that are returned from the anonymous wrapper.
+Those three public functions are closures that share the same environment.
+Thanks to JavaScript's lexical scoping, they each have access to the privateCounter variable and changeBy function.
+*/
+/*
+Using closures in this way provides a number of benefits that are normally associated with object-oriented programming --
+ in particular, data hiding and encapsulation.
+*/
+
+
+/*
+Closure Scope ChainSection
+For every closure we have three scopes:-
+    Local Scope ( Own scope)
+    Outer Functions Scope
+    Global Scope
+*/
+// NB! All closures have access to all outer function scopes within which they were declared.
+// global scope
+let e = 10;
+function sumA(a){
+  // outer functions scope
+  return function(b){
+    // outer functions scope
+    return function(c){
+      // outer functions scope
+      return function(d){
+        // local scope
+        return a + b + c + d + e;
+      }
+    }
+  }
+}
+
+aaa = sumA(1)(2)(3)(4); // log 20
+
+// We can also write with named functions:
+// global scope
+e = 30;
+function sumN(a){
+  // outer functions scope
+  return function sum2(b){
+    // outer functions scope
+    return function sum3(c){
+      // outer functions scope
+      return function sum4(d){
+        // local scope
+        return a + b + c + d + e;
+      }
+    }
+  }
+}
+var s = sumN(1); // return sum2(b) with closure 1
+var s1 = s(2); // return sum3(c) with closure 2
+var s2 = s1(3); // return sum4(d) with closure 3
+var s3 = s2(4); // return 40 = 1 + 2 + 3 + 4 + 40 (global scope e)
+aaa = s3; //log 40
+aaa = sumA(1)(2)(3)(4); //log 40
+
+// Closures in loop.
+aaa = "";
+function setupHelp() {
+  var helpText = [
+      {'id': 'email', 'help': 'Your e-mail address'},
+      {'id': 'name', 'help': 'Your full name'},
+      {'id': 'age', 'help': 'Your age (you must be over 16)'}
+    ];
+
+  for (var i = 0; i < helpText.length; i++) {
+    let item = helpText[i];
+    //aaa = aaa + function() {return item.help;}() + "; "; // with decalred anonymous function  IIFE.
+    aaa = aaa + (() => item.help)() + "; "; // with arrow function  IIFE.
+  }
+}
+setupHelp();
+
+
+
 
 return;
 
