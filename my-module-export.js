c4d5d8cb15c6f5c2a289f5ef279bfbaa95bb89f4
AKaarna <akaarna@debian930.AccountGt.net>
2018-10-17 15:44:55 +0300

    export-import

--- my-module-export.js
+++ my-module-export.js
@@ -2,19 +2,47 @@
 // Exported modules are in strict mode whether you declare them as such or not.
 // The export statement cannot be used in embedded scripts.
 
-module.exports.decr = (strGGuardEnc, strEnc) => {
-  return decrEx(strGGuardEnc, strEnc);
-};
-/*
-module.exports.decr = function(strGGuardEnc, strEnc) {
-  return decrEx(strGGuardEnc, strEnc);
-};
-*/
+// simple CommonJS module (let’s call the module 'foobar') :
+function foo() {
+  return 'hi from foo';
+}
+function bar() {
+  return 'hi from bar';
+}
+module.exports.foo = foo;
+module.exports.bar = bar;
 
-/*
-function cube(x) {
+function cubeEx(x) {
   return x * x * x;
 }
+module.exports.cube = cubeEx;
+
+/*
+//Here is the “equivalent” module written using ES6 syntax:
+export function foo() { // SyntaxError: Unexpected token export!!! in vscode.
+  return 'bar';
+}
+export function bar() {
+  return 'foo';
+}
+/*
+
+/*
+module.exports.cube = (x) => cubeEx(x);
+*/
+/*
+module.exports.cube = (x) => {
+  return cubeEx(x);
+}
+*/
+/*
+module.exports.cube = function(x) {
+  return cubeEx(x);
+}
+*/
+
+
+/*
 const foo = Math.PI + Math.SQRT2;
 var graph = {
     options:{
@@ -29,56 +57,32 @@ export { cube, foo, graph };
 */
 
 /*
-function decrEx(strGGuardEnc, strInEnc) {
-  var strOut;
-  var strGguardIn, strNumberIn; // var strJulian;
-  var strReturn, strGGuard, strTicNum, strCheckDigits;
-  var strArr; // intVar, i;
-  var strGGuardArr, strJulianArr, strTicNumArr, strCheckDigitsArr;
+// simple CommonJS module (let’s call the module 'foobar') :
+function foo() {
+  return 'bar';
+}
+function bar() {
+  return 'foo';
+}
+module.exports.foo = foo;
+module.exports.bar = bar;
 
-  //console.log(`input: ${strGGuardEnc} ${strInEnc}`);
+// Using CommonJS foobar:
+const {foo, bar} = require('foobar');
+console.log(foo());
+console.log(bar());
 
-  strOut="";
-  if (typeof(strGGuardEnc) != "string") return "ERROR2";
-  if (typeof(strInEnc) != "string") return "ERROR3";
 
-  // e.g. strGGuardEnc="123456" and strEnc = "123-12345678-1234567"
-  strGguardIn = String(strGGuardEnc);
-  strNumberIn = String(strInEnc);
-  if (strGguardIn.length != 6)  return "ERROR2";
-  if (strNumberIn.length != 20) return "ERROR3";
+//Here is the “equivalent” module written using ES6 syntax:
+export function foo() {
+  return 'bar';
+}
+export function bar() {
+  return 'foo';
+}
 
-  //intVar = Number(strGguardIn);
-  //console.log(intVar + " " + typeof(intVar));
-  if (! Number.isInteger(Number(strGguardIn))) return "ERROR2"; 
-  strArr = strNumberIn.split("-");
-  if (strArr.length != 3) return "ERROR3";
-  if (strArr[0].length != 3) return "ERROR3";
-  if (strArr[1].length != 8) return "ERROR3";
-  if (strArr[2].length != 7) return "ERROR3";
-  if (! Number.isInteger(Number(strArr[0]))) return "ERROR3"; 
-  if (! Number.isInteger(Number(strArr[1]))) return "ERROR3"; 
-  if (! Number.isInteger(Number(strArr[2]))) return "ERROR3"; 
-
-  //var strGGuardArr, strJulianArr, strTicNumArr, strChcekDigitsArr;
-  strGGuardArr = strGguardIn.split("");
-  strJulianArr = strArr[0].split("");
-  strTicNumArr = strArr[1].split("");
-  strCheckDigitsArr = strArr[2].split("");
-
-  strGGuard = addon.unld(strGGuardArr[0], strGGuardArr[1], strGGuardArr[2], strGGuardArr[3], strGGuardArr[4], strGGuardArr[5], "G", "G", strJulianArr[0], strJulianArr[1], strJulianArr[2]); // gGuard.
-  //console.log('Return string:', strGGuard + " typeof: " + typeof(strGGuard)); // gguard.
-  strTicNum = addon.unld(strTicNumArr[0], strTicNumArr[1], strTicNumArr[2], strTicNumArr[3], strTicNumArr[4], strTicNumArr[5], strTicNumArr[6], strTicNumArr[7], strJulianArr[0], strJulianArr[1], strJulianArr[2]); // external number.
-  //console.log('Return string:', strTicNum + " typeof: " + typeof(strTicNum)); // TicNum external number.
-  strCheckDigits = addon.unld(strCheckDigitsArr[0], strCheckDigitsArr[1], strCheckDigitsArr[2], strCheckDigitsArr[3], strCheckDigitsArr[4], strCheckDigitsArr[5], strCheckDigitsArr[6], "D", strJulianArr[0], strJulianArr[1], strJulianArr[2]); // check Digits.
-  //console.log('Return string:', strCheckDigits + " typeof: " + typeof(strCheckDigits)); // gguard, num, check.
-  strOut = strGGuard + " " + strArr[0] + "-" + strTicNum + "-" + strCheckDigits;
-  //console.log(`output: ${strOut}`);
-  //strArr = strIn.split("-");
-  //strOut = strArr[2] + "-" + strArr[1] + "-" + strArr[0];
-  
-  ExitFunction:
-  return strOut;
-
-} // end of function decrEx(strIn).
+// Using ES6 foobar:
+import {foo, bar} from 'foobar';
+console.log(foo());
+console.log(bar());
 */
--- my-module-import.js
+++ my-module-import.js
@@ -3,8 +3,39 @@
 // Imported modules are in strict mode whether you declare them as such or not.
 // The import statement cannot be used in embedded scripts unless such script has a type="module".
 
+
+// File is a CommonJS module; it may be converted to an ES6 module.
+// Putting it at every require is just not right, as still in many cases it's better to stick to CJS than to ESM.
+// So putting such message is quite annoying.
+// CJS is great modules format when we want to target pre ES2017 implementations without transpilation
+// (it's format that bundles directly with no syntax changes, even to ES3 engines, ESM will always require transpilation step)
+// There is no plan to remove or deprecate CJS in Node.js. ESM is being added there as a second format,
+// and due to richer functionality of CJS (e.g. public access to cache), one may still prefer to write CJS for Node.js
+// programs.
+// ESM still doesn't work reliably in many enviroments (e.g. in Node.js it's just behind experimental-flag,
+// and it's likely this implementation will change)
+
+
+// Using CommonJS foobar:
+const {foo, bar, cube} = require('./my-module-export.js');
+console.log(foo());
+console.log(bar());
+let aaa = cube(3); // 27
+
+/*
+// Using ES6 foobar:
+import {foo, bar} from 'foobar'; // SyntaxError: Unexpected token import!!! in vscode
+console.log(foo());
+console.log(bar());
+*/
+
+
+return;
+
+/*
 const myModExported = require('./my-module-export.js');
 let aaa = myModExported.cube(3); // 27
+*/
 
 /*
 import { cube, foo, graph } from 'my-module-export.js';
@@ -15,3 +46,34 @@ graph.options = {
 */
 //graph.draw();
 //console.log(foo);    // 4.555806215962888
+
+/*
+// simple CommonJS module (let’s call the module 'foobar') :
+function foo() {
+  return 'bar';
+}
+function bar() {
+  return 'foo';
+}
+module.exports.foo = foo;
+module.exports.bar = bar;
+
+// Using CommonJS foobar:
+const {foo, bar} = require('foobar');
+console.log(foo());
+console.log(bar());
+
+
+//Here is the “equivalent” module written using ES6 syntax:
+export function foo() {
+  return 'bar';
+}
+export function bar() {
+  return 'foo';
+}
+
+// Using ES6 foobar:
+import {foo, bar} from 'foobar';
+console.log(foo());
+console.log(bar());
+*/
