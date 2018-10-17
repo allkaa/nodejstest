0601567b9845bf1aa85c1f5fbe3d5af7797866ef
AKaarna <akaarna@debian930.AccountGt.net>
2018-10-17 10:18:56 +0300

    eventloop

---
+++ eventloop.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/*
+  Objects in JavaScript.
+  Primitives:
+    Number
+    String
+    Boolean
+    Symbol (new in ES2015)
+    null
+    undefined
+  Objects:    
+    Object
+    Function
+    Array
+    Date
+    RegExp
+*/
+
+
+let dtVar;
+
+dtVar = new Date()
+console.log('====> START OF PROGRAM' + " " + dtVar.getSeconds() + "." + dtVar.getMilliseconds());
+
+
+
+
+dtVar = new Date()
+console.log('====> END OF PROGRAM' + " " + dtVar.getSeconds() + "." + dtVar.getMilliseconds());
