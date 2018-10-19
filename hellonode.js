68a02d8427c1c153ec9d50f79354c2cf162bb2f6
AKaarna <akaarna@debian930.AccountGt.net>
2018-10-19 10:30:06 +0300

    express

---
+++ hellonode.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//Load HTTP module
+const http = require("http");
+const hostname = 'localhost'; //'127.0.0.1';
+const port = 3000;
+
+//Create HTTP server and listen on port 3000 for requests
+const server = http.createServer((req, res) => {
+
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`Hello World from ${hostname}:${port} \n`);
+});
+
+//Begin listening for request on port, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => { // callback function.
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+console.log('End of main program.');
