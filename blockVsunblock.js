6fbef687de84e4034536a8776868539c370740b1
AKaarna <akaarna@debian930.AccountGt.net>
2018-10-20 11:52:22 +0300

    block

--- blockVsunblock.js
+++ blockVsunblock.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const fs = require('fs');
-const data = fs.readFileSync('/package.json'); // blocks here until file is read
-console.log(data);
-// moreWork(); will run after console.log
+
+//const data = fs.readFileSync('./package.json'); // blocks here until file is read
+
+fs.readFile('./package.json', (err, data) => {
+  if (err) throw err;
+  else {
+    console.log(data);
+    let aaa;
+    aaa = '';
+    for (let i=0; i<data.length; i++) {
+      aaa = aaa + String.fromCharCode(data[i]);
+    }
+    console.log(aaa);
+  }
+});
+// moreWork(); will run before console.log
+
+console.log("End of main Program.")
 return;
