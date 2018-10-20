2055b560df3b8541749ff42b6f139bcc4cf250bd
AKaarna <akaarna@debian930.AccountGt.net>
2018-10-20 17:17:37 +0300

    block

--- blockVsunblock.js
+++ blockVsunblock.js
@@ -1,9 +1,12 @@
 'use strict';
 
+console.log("Start of main Program " + Date.now())
+
 const fs = require('fs');
 
 //const data = fs.readFileSync('./package.json'); // blocks here until file is read
 
+/*
 fs.readFile('./package.json', (err, data) => {
   if (err) throw err;
   else {
@@ -17,6 +20,35 @@ fs.readFile('./package.json', (err, data) => {
   }
 });
 // moreWork(); will run before console.log
+*/
 
-console.log("End of main Program.")
+function someAsyncOperation(callback) {
+  // Assume this takes 95ms to complete
+  console.log("Start of fs.readFile " + Date.now())
+  fs.readFile('./package.json', callback);
+}
+
+const timeoutScheduled = Date.now();
+console.log("Set timeout " + timeoutScheduled)
+setTimeout(() => {
+  console.log("Start timeout callback " + Date.now())
+  const delay = Date.now() - timeoutScheduled;
+  console.log(`${delay}ms have passed since I was scheduled`);
+}, 100);
+
+
+// do someAsyncOperation which takes 95 ms to complete
+someAsyncOperation(() => {
+  const startCallback = Date.now();
+  console.log('callback started ' + startCallback);
+  // do something that will take 10ms...
+  while (Date.now() - startCallback < 10) {
+    // do nothing
+  }
+  console.log('callback ended ' + Date.now());
+});
+
+
+
+console.log("End of main Program " + Date.now())
 return;
