9a8d0715dc0f3110e50bed80544558443491c0a6
AKaarna <akaarna@debian930.AccountGt.net>
2018-10-21 16:17:16 +0300

    modif templaate

--- urlParseNew.js
+++ urlParseNew.js
@@ -47,7 +47,7 @@ server.on('request', (req, res) => { // request is <http.IncomingMessage>, respo
   let objUrl = urlLegacy.parse(req.url, true, true);
   let q = objUrl.query; // parsed url query property object e.g. { fist: '1', second: '2' }.
   //let objParsedNew = new URL(req.url);
-  if (objUrl.search == "") {
+  if (objUrl.search == "") { // no query /?first=1...  case.
     if (objUrl.pathname.includes('/styles/')) {
       fs.readFile('.' + objUrl.pathname, (err, data) => { //'.' + "/styles/style.css"
         if (err) throw err;
@@ -89,7 +89,7 @@ server.on('request', (req, res) => { // request is <http.IncomingMessage>, respo
         }
       });
     }
-    else {
+    else { // default index.html.
       fs.readFile('./index.html', (err, data) => {
         if (err) throw err;
         else {
@@ -99,34 +99,38 @@ server.on('request', (req, res) => { // request is <http.IncomingMessage>, respo
         }
       });
     }
-  }
-  else {
-    console.log("parsed url query property object:");
-    console.log(q);
-    let txt = '';
-    for (let key in q) {
-      txt = txt + key + ":" + q[key] + '<br />';
-    }
-    let dtVar2 = new Date();
-    let msg;
-    //res.write('<result><br />|<br />' + txt + '<br />' + dtVar2.getSeconds() + "." + dtVar2.getMilliseconds() +'<br />|</result>');
-    msg = '<!DOCTYPE html><html>';
-    msg = msg + ' <head><meta charset="utf-8"><title>My test page</title>';
-    msg = msg + `<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>`;
-    msg = msg + `<link href="styles/style.css" rel="stylesheet" type="text/css"></head>`;
-    msg = msg + `<body>`;
-    msg = msg + `<h1>Mozilla is cool</h1>`;
-    //file:///home/akaarna/nodejs/images/firefox-icon.png
-    //msg = msg + `<img src="https://findicons.com/files/icons/783/mozilla_pack/128/firefox.png" alt="Firefox logo: a flaming fox surrounding the Earth." width="200" >`;
-    //msg = msg + `<img src="file:///home/akaarna/nodejs/images/firefox-icon.png" alt="Firefox logo: a flaming fox surrounding the Earth." width="200" >`;
-    msg = msg + `<img src="images/firefox-icon.png" alt="Firefox logo: a flaming fox surrounding the Earth." width="200" >`;
-    msg = msg + '<br />' + txt;
-    msg = msg + `</body></html>`;
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(msg);
-    res.end();
-  }
-})
+  } // end of // no query /?first=1...  case.
+  else { // modifying default index.html template with query params.
+    fs.readFile('./index.html', (err, data) => { // file index.html reading.
+      if (err) throw err;
+      else { // file index.html read ok.
+        //console.log("parsed url query property object:");
+        //console.log(q);
+        let txt = '';
+        for (let key in q) {
+          txt = txt + key + ":" + q[key] + '<br />';
+        }
+        //let dtVar2 = new Date();
+        let msg;
+        msg = '<!DOCTYPE html><html>';
+        msg = msg + ' <head><meta charset="utf-8"><title>My test page</title>';
+        msg = msg + `<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>`;
+        msg = msg + `<link href="styles/style.css" rel="stylesheet" type="text/css"></head>`;
+        msg = msg + `<body>`;
+        msg = msg + `<h1>Mozilla is cool</h1>`; // NB! h1 will be changed from client script.
+        //msg = msg + `<img src="https://findicons.com/files/icons/783/mozilla_pack/128/firefox.png" alt="Firefox logo: a flaming fox surrounding the Earth." width="200" >`;
+        msg = msg + `<img src="images/firefox-icon.png" alt="Firefox logo: a flaming fox surrounding the Earth." width="200" >`;
+        msg = msg + `<p>Read <a href="https://www.mozilla.org/en-US/about/manifesto/">Mozilla Manifesto</a> for more...</p>`;
+        msg = msg + '<br />' + txt;
+        msg = msg + `<script src="scripts/main.js"></script>`;
+        msg = msg + `</body></html>`;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(msg);
+        res.end();
+      } // end  of file index.html read OK.
+    }); // end of file index.html reading.
+  } // end of modifying default index.html template with query params.
+}) // end of server.on('request'...)
 
 dtVar = new Date();
 console.log('after http.createServer ' + dtVar.getSeconds() + "." + dtVar.getMilliseconds());
