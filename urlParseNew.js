ded5fdf2f9e3b07a490d22ec61853996050c21ca
AKaarna <akaarna@debian930.AccountGt.net>
2018-10-21 11:21:54 +0300

    style

--- urlParseNew.js
+++ urlParseNew.js
@@ -1,12 +1,11 @@
 'use strict';
 
-let http = require('http');
-//let formidable = require('formidable');
-let fs = require('fs');
-let qs = require('querystring');
-
-const hostname = 'localhost';
-const port = 8081; // process.env.Port;
+const http = require('http');
+const urlLegacy = require('url');
+//const { URL } = require('url');
+const fs = require('fs');
+//const formidable = require('formidable');
+//const qs = require('querystring');
 
 let dtVar = new Date();
 console.log('Program starts ==================================== ' + dtVar.getSeconds() + "." + dtVar.getMilliseconds());
@@ -17,11 +16,11 @@ for (let prop in process.env) {
 dtVar = new Date();
 console.log('==================================== ' + dtVar.getSeconds() + "." + dtVar.getMilliseconds());
 
-dtVar = new Date();
+const hostname = 'localhost';
 //console.log('set port=' + process.env.PORT + " " + dtVar.getSeconds() + "." + dtVar.getMilliseconds());
 //var port = process.env.PORT; //  Windows - default port is 1337 for WebApp and 1542 for ConsoleApp;
-let port = 8081; // Linux - no default port.
-console.log('set port=' + port + " " + dtVar.getSeconds() + "." + dtVar.getMilliseconds());
+const port = 8081; // process.env.Port;
+console.log('set port=' + port);
 
 dtVar = new Date();
 console.log('before http.createServer() ' + dtVar.getSeconds() + "." + dtVar.getMilliseconds());
@@ -39,35 +38,44 @@ server.on('request', (req, res) => { // request is <http.IncomingMessage>, respo
   // The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays,
   // or properties from objects, into distinct variables.
   const { method, url, headers } = req;
-  const responseBody = { headers, method, url };
-  const responceBodyStringify = JSON.stringify(responseBody);
-  //if (req.url.includes('/fileupload')) { // (req.url == '/fileupload')
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  let objParsed = url.parse(req.url, true);
-  let q = objParsed.query; // parsed url query property object e.g. { f: '1', s: '2' }.
-  console.log("parsed url query property object:");
-  console.log(q);
-  let txt = '';
-  for (let key in q) {
-    txt = txt + key + ":" + q[key] + '<br />';
+  //const responseBody = { headers, method, url };
+  //const responceBodyStringify = JSON.stringify(responseBody);
+  // http://localhost:8081/home/akaarna/nodejs/images/?first=1&second=2 // URL object objUrl.search = "?first=1&second=2"
+  // or req.url = "/home/akaarna/nodejs/images/images/firefox-icon.png" // URL object objUrl.search = ""
+  // objUrl.pathname = "/images/firefox-icon.png"
+  let aaa = new Object();
+  let objUrl = urlLegacy.parse(req.url, true, true);
+  let q = objUrl.query; // parsed url query property object e.g. { fist: '1', second: '2' }.
+  //let objParsedNew = new URL(req.url);
+  if (objUrl.search == "") {
+
   }
-  let dtVar2 = new Date();
-  let msg;
-  //res.write('<result><br />|<br />' + txt + '<br />' + dtVar2.getSeconds() + "." + dtVar2.getMilliseconds() +'<br />|</result>');
-  msg = '<!DOCTYPE html><html>';
-  msg = msg + ' <head><meta charset="utf-8"><title>My test page</title>';
-  msg = msg + `<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>`;
-  //msg = msg + `<link href="styles/style.css" rel="stylesheet" type="text/css"></head>`;
-  msg = msg + `<body>`;
-  msg = msg + `<h1>Mozilla is cool</h1>`;
-  //file:///home/akaarna/nodejs/images/firefox-icon.png
-  //msg = msg + `<img src="https://findicons.com/files/icons/783/mozilla_pack/128/firefox.png" alt="Firefox logo: a flaming fox surrounding the Earth." width="200" >`;
-  //msg = msg + `<img src="file:///home/akaarna/nodejs/images/firefox-icon.png" alt="Firefox logo: a flaming fox surrounding the Earth." width="200" >`;
-  msg = msg + `<img src="images/firefox-icon.png" alt="Firefox logo: a flaming fox surrounding the Earth." width="200" >`;
-  msg = msg + '<br />' + txt;
-  msg = msg + `</body></html>`;
-  res.write(msg);
-  res.end();
+  else {
+    console.log("parsed url query property object:");
+    console.log(q);
+    let txt = '';
+    for (let key in q) {
+      txt = txt + key + ":" + q[key] + '<br />';
+    }
+    let dtVar2 = new Date();
+    let msg;
+    //res.write('<result><br />|<br />' + txt + '<br />' + dtVar2.getSeconds() + "." + dtVar2.getMilliseconds() +'<br />|</result>');
+    msg = '<!DOCTYPE html><html>';
+    msg = msg + ' <head><meta charset="utf-8"><title>My test page</title>';
+    msg = msg + `<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>`;
+    //msg = msg + `<link href="styles/style.css" rel="stylesheet" type="text/css"></head>`;
+    msg = msg + `<body>`;
+    msg = msg + `<h1>Mozilla is cool</h1>`;
+    //file:///home/akaarna/nodejs/images/firefox-icon.png
+    //msg = msg + `<img src="https://findicons.com/files/icons/783/mozilla_pack/128/firefox.png" alt="Firefox logo: a flaming fox surrounding the Earth." width="200" >`;
+    //msg = msg + `<img src="file:///home/akaarna/nodejs/images/firefox-icon.png" alt="Firefox logo: a flaming fox surrounding the Earth." width="200" >`;
+    msg = msg + `<img src="images/firefox-icon.png" alt="Firefox logo: a flaming fox surrounding the Earth." width="200" >`;
+    msg = msg + '<br />' + txt;
+    msg = msg + `</body></html>`;
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(msg);
+    res.end();
+  }
 })
 
 dtVar = new Date();
@@ -78,7 +86,7 @@ console.log('after http.createServer ' + dtVar.getSeconds() + "." + dtVar.getMil
 // or the unspecified IPv4 address (0.0.0.0) otherwise.
 server.listen(port, hostname, () => {
   // Place holders in template literals are indicated by the $ (Dollar sign) and curly braces e.g. (${expression}).
-  console.log(`Server running and listening at http://${hostname}:${port}/`); // ${expression} is place holders in template literal enclosed by the back-tick (` `) (grave accent) characters.
+  console.log(`server.listen: Server running and listening at http://${hostname}:${port}/`); // ${expression} is place holders in template literal enclosed by the back-tick (` `) (grave accent) characters.
 });
 
 dtVar = new Date();
