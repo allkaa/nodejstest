02a5fd2f10f2fedd4525dc041cb347d6c028d789
AKaarna <akaarna@debian930.AccountGt.net>
2018-10-22 12:07:53 +0300

    formInputGetPost

--- formInputText.js
+++ formInputText.js
@@ -1,13 +1,16 @@
 'use strict';
 
-var http = require('http');
-//var formidable = require('formidable');
-//var fs = require('fs');
-var qs = require('querystring');
-const hostname = 'localhost';
-var port = 8081; //process.env.port || 1337;
+const http = require('http');
+const urlLegacy = require('url');
+const fs = require('fs');
+const qs = require('querystring');
+//const formidable = require('formidable');
 
-var server = http.createServer(function (req, res) {
+const hostname = 'localhost';
+const port = 8081; //process.env.port || 1337;
+
+const server = http.createServer();
+server.on('request', (req, res) => { // request is <http.IncomingMessage>, response is <http.ServerResponse>.
   req.on('error', (err) => {
     // This prints the error message and stack trace to `stderr`.
     console.log('NB!!! Nodejs server request error message and stack treace to strerr');
@@ -20,40 +23,81 @@ var server = http.createServer(function (req, res) {
     console.log('NB!!! Nodejs server response error message and stack treace to strerr');
     console.error(err.stack);
   });
-  if (req.url.includes('/submitTest')) { // (req.url == '/submitTest')
-    var body = '';
-    req.on('data', function (data) {
-      body += data;
-      // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB.
-      if (body.length > 1e6) {
-        // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST.
-        req.connection.destroy();
+  if (req.url.includes('/submitForm')) { // For method="post" req.url = "/submitForm", for method="get" e.g. req.url = "/submitForm?fname=alex&sname=Raven"
+    if (req.method == "POST") {
+      let body = '';
+      req.on('data', function (data) {
+        body += data;
+        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB.
+        if (body.length > 1e6) {
+          // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST.
+          req.connection.destroy();
+        }
+      });
+      req.on('end', function () {
+        // e.g. body = 'fname=Alex\r\nsname=Raven\r\n'
+        console.log(body);
+        var strVar = '';
+        for (var i = 0; i < body.length; i++) {
+          strVar = strVar + body.charCodeAt(i) + ",";
+        }
+        console.log(strVar);
+        var objBody = qs.parse(body, "\r\n", "=");
+        console.log(objBody);
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(`<htm>`);
+        res.write(`<body>`);
+        res.write(`<form action="submitForm" method="post" enctype="text/plain">`); // method="post"
+        res.write(`First name:<br /> <input type="text" name="fname"><br />`);
+        res.write(`Last name:<br /> <input type="text" name="sname"><br />`);
+        res.write(`<br /><input type="submit">`);
+        res.write(`</form>`);
+        res.write('form ' + req.method + ' method info read' + '<br />');
+        res.write('Name = ' + objBody.fname + '<br />');
+        res.write('Surname = ' + objBody.sname);
+        res.write(`</body>`);
+        res.write(`</htm>`);
+          return res.end();
+      });
       }
-    });
-    req.on('end', function () {
-      // e.g. body = 'fname=Al\r\nsname=Kaarna\r\n'
-      console.log(body);
-      var strVar = '';
-      for (var i = 0; i < body.length; i++) {
-        strVar = strVar + body.charCodeAt(i) + ",";
+    else if (req.method = "GET") {
+      let objUrl = urlLegacy.parse(req.url, true, true);
+      let q = objUrl.query; // parsed url query property object e.g. { fist: '1', second: '2' }.
+      //console.log("parsed url query property object:");
+      //console.log(q);
+      /*
+      let txt = '';
+      for (let key in q) {
+        txt = txt + key + ":" + q[key] + '<br />';
       }
-      console.log(strVar);
-      var objBody = qs.parse(body, "\r\n", "=");
-      console.log(objBody);
+      */
       res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(`<htm>`);
+      res.write(`<body>`);
+      res.write(`<form action="submitForm" method="get" enctype="text/plain">`); // method="post"
+      res.write(`First name:<br /> <input type="text" name="fname"><br />`);
+      res.write(`Last name:<br /> <input type="text" name="sname"><br />`);
+      res.write(`<br /><input type="submit">`);
+      res.write(`</form>`);
       res.write('form ' + req.method + ' method info read' + '<br />');
-      res.write('Name = ' + objBody.fname + '<br />');
-      res.write('Surname = ' + objBody.sname);
+      res.write('Name = ' + q.fname + '<br />');
+      res.write('Surname = ' + q.sname);
+      res.write(`</body>`);
+      res.write(`</htm>`);
       return res.end();
-    });
+    }
   }
   else {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<form action="submitTest" method="post" enctype="text/plain">'); // method="post"
-    res.write('First name:<br /> <input type="text" name="fname"><br />');
-    res.write('Last name:<br /> <input type="text" name="sname"><br />');
-    res.write('<input type="submit">');
-    res.write('</form>');
+    res.write(`<htm>`);
+    res.write(`<body>`);
+    res.write(`<form action="submitForm" method="post" enctype="text/plain">`); // method="post" or method="get",
+    res.write(`First name:<br /> <input type="text" name="fname"><br />`);
+    res.write(`Last name:<br /> <input type="text" name="sname"><br />`);
+    res.write(`<br /><input type="submit">`);
+    res.write(`</form>`);
+    res.write(`</body>`);
+    res.write(`</htm>`);
     return res.end();
   }
 });
