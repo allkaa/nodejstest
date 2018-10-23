69f2190014dff6580e37c56439c2d722d9a81f9a
AKaarna <akaarna@debian930.AccountGt.net>
2018-10-23 10:39:18 +0300

    nodeServerGetAndPost

--- nodeServer.js
+++ nodeServer.js
@@ -8,7 +8,7 @@ const qs = require('querystring');
 //const formidable = require('formidable');
 
 let dtVar = new Date();
-console.log('Program starts ==================================== ' + dtVar.getSeconds() + "." + dtVar.getMilliseconds());
+console.log('Program starts ' + dtVar.getSeconds() + "." + dtVar.getMilliseconds());
 /*
 //var envObj = process.env;
 for (let prop in process.env) {
@@ -40,9 +40,11 @@ server.on('request', (req, res) => { // request is <http.IncomingMessage>, respo
   // or properties from objects, into distinct variables.
   const { method, url, headers } = req;
   //let aaa = new Object();
-  let objUrl = urlLegacy.parse(req.url, true, true);
+  // url "/submitFormAK?fname=Alex&sname=Raven" or "/"
+  let objUrl = urlLegacy.parse(req.url, true, true); // non standard object.s
   // Verify that it is very first page request or rendering page after GET or POST form submit processed.
   // After POST form submit will be processed rendering page will be as GET.
+  // objUrl.search is ? and query e.g. "?fname=Alex&sname=Raven" or ""
   if ((req.method != "POST") && (objUrl.search == "")) { // if req.method == "POST" then ObjUrl.search will be "" always.
     if (objUrl.pathname.includes('/styles/')) {
       fs.readFile('.' + objUrl.pathname, (err, data) => { //'.' + "/styles/style.css"
@@ -84,94 +86,101 @@ server.on('request', (req, res) => { // request is <http.IncomingMessage>, respo
         }
       });
       }
-  } // end of // no query /?first=1...  case.
+  } // end of empty search ""  case.
   // <==================== end of very first case and rendering results case ================================================>
-  // Begin of POST or GET form submit case.
-  if (req.url.includes('/submitFormAK')) { // For method="post" req.url = "/submitFormAK", for method="get" e.g. req.url = "/submitFormAK?fname=Alex&sname=Raven"
-    if (req.method == "POST") {
-      let body = '';
-      req.on('data', function (data) {
-        body += data;
-        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB.
-        if (body.length > 1e6) {
-          // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST.
-          req.connection.destroy();
-        }
-      });
-      req.on('end', function () {
-        // e.g. body = 'fname=Alex\r\nsname=Raven\r\n'
-        /*
-        console.log(body);
-        let strVar = '';
-        for (let i = 0; i < body.length; i++) {
-          strVar = strVar + body.charCodeAt(i) + ",";
-        }
-        console.log(strVar);
-        */
-        let objBody = qs.parse(body, "\r\n", "="); // using const qs = require('querystring') module.
-        //console.log(objBody);
-        fs.readFile('./index.html', (err, data) => { // index.html reading.
+  // Non empty objUrl.search (is ? and query) e.g. "?fname=Alex&sname=Raven"
+  else {
+    // Begin of POST or GET form submit case.
+    if (req.url.includes('/submitFormAK')) { // For method="post" req.url = "/submitFormAK", for method="get" e.g. req.url = "/submitFormAK?fname=Alex&sname=Raven"
+      if (req.method == "POST") {
+        let body = '';
+        req.on('data', function (data) {
+          body += data;
+          // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB.
+          if (body.length > 1e6) {
+            // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST.
+            req.connection.destroy();
+          }
+        });
+        req.on('end', function () {
+          // e.g. body = 'fname=Alex\r\nsname=Raven\r\n'
+          /*
+          console.log(body);
+          let strVar = '';
+          for (let i = 0; i < body.length; i++) {
+            strVar = strVar + body.charCodeAt(i) + ",";
+          }
+          console.log(strVar);
+          */
+          let objBody = qs.parse(body, "\r\n", "="); // using const qs = require('querystring') module.
+          //console.log(objBody);
+          fs.readFile('./index.html', (err, data) => { // index.html reading.
+            if (err) throw err;
+            else { // file index.html read OK -  modify template file.
+              let msgOrig = '';
+              for (let i=0; i<data.length; i++) {
+                msgOrig = msgOrig + String.fromCharCode(data[i]);
+              }
+              let msg = msgOrig.substring(0,msgOrig.indexOf(`</body>`));
+              msg += 'LAST ENTERED by method ' + req.method + ':<br />';
+              if (objBody.fname == "") {
+                msg += 'Name = ' + 'not entered!' + '<br />';
+              }
+              else {
+                msg += 'Name = ' + objBody.fname + '<br />';
+              }
+              if (objBody.sname == "") {
+                msg += 'Surname = ' + 'not entered!' + '<br />';
+              }
+              else {
+                msg += 'Surname = ' + objBody.sname + '<br />';
+              }
+              msg += msgOrig.substring(msgOrig.indexOf(`</body>`));
+              res.writeHead(200, { 'Content-Type': 'text/html' });
+              res.write(msg);
+              return res.end();
+            } // end  of file index.html read OK - modify template file.
+          }); // end of file index.html reading.
+        }); // end req.on('end', function ()...
+      } // end of if req.method == "POST".
+      // <==================================== end of POST, begin of GET =====================================>
+      else if (req.method = "GET") {
+        let q = objUrl.query; // formerly parsed query property object e.g. Object {fname: "Alex", sname: "Raven"}.
+        fs.readFile('./index.html', (err, data) => { // file index.html reading.
           if (err) throw err;
-          else { // file index.html read OK -  modify template file.
+          else { // file index.html read OK - modify template file.
             let msgOrig = '';
             for (let i=0; i<data.length; i++) {
               msgOrig = msgOrig + String.fromCharCode(data[i]);
             }
             let msg = msgOrig.substring(0,msgOrig.indexOf(`</body>`));
             msg += 'LAST ENTERED by method ' + req.method + ':<br />';
-            if (objBody.fname == "") {
+            if (q.fname == "") {
               msg += 'Name = ' + 'not entered!' + '<br />';
             }
             else {
-              msg += 'Name = ' + objBody.fname + '<br />';
+              msg += 'Name = ' + q.fname + '<br />';
             }
-            if (objBody.sname == "") {
+            if (q.sname == "") {
               msg += 'Surname = ' + 'not entered!' + '<br />';
             }
             else {
-              msg += 'Surname = ' + objBody.sname + '<br />';
+              msg += 'Surname = ' + q.sname + '<br />';
             }
-              msg += msgOrig.substring(msgOrig.indexOf(`</body>`));
+            msg += msgOrig.substring(msgOrig.indexOf(`</body>`));
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(msg);
             return res.end();
           } // end  of file index.html read OK - modify template file.
         }); // end of file index.html reading.
-      });
-    } // end of if req.method == "POST".
-    // <==================================== end of POST, begin of GET =====================================>
-    else if (req.method = "GET") {
-      let q = objUrl.query; // formerly parsed query property object e.g. Object {fname: "Alex", sname: "Raven"}.
-      fs.readFile('./index.html', (err, data) => { // file index.html reading.
-        if (err) throw err;
-        else { // file index.html read OK - modify template file.
-          let msgOrig = '';
-          for (let i=0; i<data.length; i++) {
-            msgOrig = msgOrig + String.fromCharCode(data[i]);
-          }
-          let msg = msgOrig.substring(0,msgOrig.indexOf(`</body>`));
-          msg += 'LAST ENTERED by method ' + req.method + ':<br />';
-          if (q.fname == "") {
-            msg += 'Name = ' + 'not entered!' + '<br />';
-          }
-          else {
-            msg += 'Name = ' + q.fname + '<br />';
-          }
-          if (q.sname == "") {
-            msg += 'Surname = ' + 'not entered!' + '<br />';
-          }
-          else {
-            msg += 'Surname = ' + q.sname + '<br />';
-          }
-          msg += msgOrig.substring(msgOrig.indexOf(`</body>`));
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.write(msg);
-          return res.end();
-        } // end  of file index.html read OK - modify template file.
-      }); // end of file index.html reading.
-    } // end of req.method = "GET".
-  } // end of if req.url.includes('/submitForm...')
-  // <==================================== End of POST or GET form submit case.  =====================================>
+      } // end of req.method = "GET".
+    } // end of if req.url.includes('/submitForm...')
+    else { // no "/submitFormAK" but search is not emty e.g. "?fname=Alex&sname=Raven"
+      // HACKER ATTACK OR FAULTY CLIENT.
+      req.connection.destroy();
+    }
+    // <==================================== End of POST or GET form submit case.  =====================================>
+  } // end of Non empty objUrl.search (is ? and query) e.g. "?fname=Alex&sname=Raven"
 }) // end of server.on('request'...)
 
 dtVar = new Date();
@@ -182,7 +191,7 @@ console.log('after http.createServer ' + dtVar.getSeconds() + "." + dtVar.getMil
 // or the unspecified IPv4 address (0.0.0.0) otherwise.
 server.listen(port, hostname, () => {
   // Place holders in template literals are indicated by the $ (Dollar sign) and curly braces e.g. (${expression}).
-  console.log(`server.listen: Server running and listening at http://${hostname}:${port}/ ` + dtVar.getSeconds() + "." + dtVar.getMilliseconds()); // ${expression} is place holders in template literal enclosed by the back-tick (` `) (grave accent) characters.
+  console.log(`Server running and listening at http://${hostname}:${port}/ ` + dtVar.getSeconds() + "." + dtVar.getMilliseconds()); // ${expression} is place holders in template literal enclosed by the back-tick (` `) (grave accent) characters.
 });
 
 dtVar = new Date();
