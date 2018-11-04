22e30bc6348441f1fd868e986c11e36af0b2da16
AKaarna <akaarna@debian930.AccountGt.net>
2018-11-04 16:32:41 +0200

    stream

--- stream.js
+++ stream.js
@@ -11,8 +11,42 @@ for (let prop in envObj) {
 //dtVar = new Date();
 //console.log('====================================' + " " + dtVar.getSeconds() + "." + dtVar.getMilliseconds());
 
+// Streams can be readable, writable, or both. All streams are instances of EventEmitter.
+const stream = require('stream'); // accessing stream module.
+// Normally all streams created by Node.js APIs operate exclusively on strings and Buffer (or Uint8Array) objects.
+// Stream instances may be switched into object mode using the objectMode option when the stream is created.
 
+// using streams in a Node.js application that implements an HTTP server.
+const http = require('http');
+const server = http.createServer((req, res) => {
+  // req is an http.IncomingMessage, which is a Readable Stream.
+  // res is an http.ServerResponse, which is a Writable Stream.
 
+  let body = '';
+  // Get the data as utf8 strings.
+  // If an encoding is not set, Buffer objects will be received.
+  req.setEncoding('utf8'); // Uint8Array
+
+  // Readable streams emit 'data' events once a listener is added
+  req.on('data', (chunk) => {
+    body += chunk;
+  });
+
+  // the end event indicates that the entire body has been received
+  req.on('end', () => {
+    try {
+      const data = JSON.parse(body);
+      // write back something interesting to the user:
+      res.write(typeof data);
+      res.end();
+    } catch (er) {
+      // uh oh! bad json!
+      res.statusCode = 400;
+      return res.end(`error: ${er.message}`);
+    }
+  });
+});
+server.listen(1337);
 
 
 dtVar = new Date();
