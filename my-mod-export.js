8139fef7d9bdc5ab3708c8f89f239e95780b0a2d
AKaarna <akaarna@debian930.AccountGt.net>
2018-11-11 12:44:33 +0200

    mymodexp

--- my-mod-export.js
+++ my-mod-export.js
@@ -2,6 +2,8 @@
 // Exported modules are in strict mode whether you declare them as such or not.
 // The export statement cannot be used in embedded scripts.
 
-// simple CommonJS module
-module.exports = (param) => {return console.log(param)}
-
+// simple CommonJS module. Export arrow function with parameter.
+module.exports = (param) => {
+  console.log(param);
+  return param;
+}
--- my-module-import.js
+++ my-module-import.js
@@ -17,8 +17,7 @@
 
 
 // Using CommonJS foobar:
-const test = require('./my-mod-export.js')('express-locallibrary-tutorial:server');
-return;
+const test = require('./my-mod-export.js')('express-locallibrary-tutorial:server'); // require and immediately execute imported function.
 const {foo, bar, cube} = require('./my-module-export.js');
 console.log(foo());
 console.log(bar());
