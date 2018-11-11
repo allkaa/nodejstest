508f44e654e014ff29f3936b7d6d00aa5c6d6db4
AKaarna <akaarna@debian930.AccountGt.net>
2018-11-11 12:27:26 +0200

    mymodexport

---
+++ styles/my-mod-export.js
@@ -0,0 +1,7 @@
+'use strict';
+// Exported modules are in strict mode whether you declare them as such or not.
+// The export statement cannot be used in embedded scripts.
+
+// simple CommonJS module
+module.exports = (param) => {return console.log(param)}
+
